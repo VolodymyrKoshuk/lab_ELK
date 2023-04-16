e44969ca7926e5dcf9586ab849c3d51d89e6faac
volodymyr_koshuk <vovakos2001@gmail.com>
2023-04-16 20:09:52 +0300

    Init commit and added file main.tf

---
+++ main.tf
@@ -0,0 +1,40 @@
+data "aws_ami" "amazon_linux2" {
+  owners = ["137112412989"]
+  most_recent = true
+
+  filter {
+    name = "name"
+    value = ["amzn2-ami-kernel-5.10-*"]
+  }
+}
+
+
+module "ec2-instance-public" {
+  source   = "terraform-aws-modules/ec2-instance/aws"
+  version  = "4.3.0"
+  for_each = toset(["first"]) # Meta-argument to create multiple servers
+
+  name     = "ELK AWS Linux #${each.value}"
+
+  ami                         = data.amazon_linux2.id
+  instance_type               = "t3.micro"
+  key_name                    = "vova-key-linuxaws-prod-stokholm"
+  associate_public_ip_address = true
+  iam_instance_profile        = "AmazonSSMRoleForInstancesQuickSetup"
+ 
+  putin_khuylo                = true
+
+  root_block_device = [
+    {
+      volume_type           = "gp3"
+      volume_size           = "10"
+      delete_on_termination = "true"
+    }
+  ]
+
+
+  tags = {
+    Terraform = true
+    Task = "Check ELK stack"
+  }
+}
